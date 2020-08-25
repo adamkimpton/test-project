a7aa1bb520b5eb2d9c1d440821e8f39dc34ff740
adamkimpton <ageofkimptown@gmail.com>
2020-08-25 17:23:38 +1200

    RAD-110: formatted with spotless apply

--- src/main/java/com/adam/testproject/Main.java
+++ src/main/java/com/adam/testproject/Main.java
@@ -9,27 +9,25 @@ package com.adam.testproject;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class Main {
     public static void Main(String[] args) {
-//        System.out.println("Hello, world!");
-//        try (BufferedReader reader =
-//                Files.newBufferedReader(Paths.get("book.csv"), Charset.defaultCharset())) {
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                String[] values = line.split(",");
-//                System.out.println(values[1]);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        //        System.out.println("Hello, world!");
+        //        try (BufferedReader reader =
+        //                Files.newBufferedReader(Paths.get("book.csv"), Charset.defaultCharset()))
+        // {
+        //            String line;
+        //            while ((line = reader.readLine()) != null) {
+        //                String[] values = line.split(",");
+        //                System.out.println(values[1]);
+        //            }
+        //        } catch (IOException e) {
+        //            e.printStackTrace();
+        //        }
 
-        List<MyBean> beans = new CsvToBeanBuilder(FileReader("yourfile.csv"))
-                .withType(Visitors.class).build().parse();
+        List<MyBean> beans =
+                new CsvToBeanBuilder(FileReader("yourfile.csv"))
+                        .withType(Visitors.class)
+                        .build()
+                        .parse();
     }
 }
